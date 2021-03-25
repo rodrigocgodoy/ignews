e6ff3188bd2f0f7954cdf80eb888436edcaee506
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2021-03-24 22:41:07 -0300

    Modify pages and components, added images and favicon

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { SignInButton } from '../SignInButton/index';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+};
